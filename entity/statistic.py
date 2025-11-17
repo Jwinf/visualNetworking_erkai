6b431b692dbba92a1e0b4851ebce1f7b7c25622c
jwinf_91 <13675006110@163.com>
2025-11-17 14:32:47 +0800

    fun: 记录时间序列

--- entity/statistic.py
+++ entity/statistic.py
@@ -1,4 +1,6 @@
 class Flow:
-    def __init__(self, in_num, out_num):
+    def __init__(self, in_num, out_num, data_hourly, data_period):
         self.in_num = in_num
         self.out_num = out_num
+        self.data_hourly = data_hourly
+        self.data_period = data_period
