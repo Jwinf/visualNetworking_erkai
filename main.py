e1b50d3b1101a7f462fcee5e4167905420f2e596
jwinf_91 <13675006110@163.com>
2025-11-17 10:50:06 +0800

    fun: 实现计数逻辑

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from util.data_process_utils import compact, get_point_x_list_by_state, find_monotonic_sequence
-from const.colors_enum import Colors
+from util.data_process_utils import compact, get_point_x_list_by_state, find_monotonic_sequence, is_point_above_line_vector, is_point_below_line_vector
+from const.monotonic_type_enum import Monotonic
+from entity.door import Door
+from entity.statistic import Flow
 
 RED = '\033[91m'
 GREEN = '\033[92m'
@@ -8,7 +10,7 @@ def banner():
     with open('./favicon.txt', 'r') as f:
         print(f.read())
 
-def counter(feature_map, state=0):
+def counter(feature_map, door, flow):
     """
     param: feature_map 数据字典<track_id, seq>; state 场景状态[0,3]
     情况1：判断p1横坐标单调性，p1递增且p1位于门下方为进门；
@@ -21,24 +23,52 @@ def counter(feature_map, state=0):
           p2递减且p2位于门下方为进门。 state = 3
     """
     for track_id in feature_map:
-        op_list = get_point_x_list_by_state(feature_map, track_id, state)
+        op_list = get_point_x_list_by_state(feature_map, track_id, door.state)
         start_index, end_index, monotonic_type = find_monotonic_sequence(op_list)
         while end_index < len(op_list) - 1:
-            match state:
+            frame = feature_map[track_id][op_list[end_index][1]]
+            match door.state:
                 case 0:
-                    pass
+                    if monotonic_type == Monotonic.INCREASING and is_point_below_line_vector(frame[0][0], frame[0][1], door.x1, door.y1, door.x2, door.y2):
+                        # 进门数+1
+                        flow.in_num += 1
+                    elif monotonic_type == Monotonic.DECREASING and is_point_above_line_vector(frame[2][0], frame[2][1], door.x1, door.y1, door.x2, door.y2):
+                        # 出门数+1
+                        flow.out_num += 1
                 case 1:
-                    pass
+                    if monotonic_type == Monotonic.INCREASING and is_point_below_line_vector(frame[0][0], frame[0][1], door.x1, door.y1, door.x2, door.y2):
+                        # 出门数+1
+                        flow.out_num += 1
+                    elif monotonic_type == Monotonic.DECREASING and is_point_above_line_vector(frame[2][0], frame[2][1], door.x1, door.y1, door.x2, door.y2):
+                        # 进门数+1
+                        flow.in_num += 1
                 case 2:
-                    pass
+                    if monotonic_type == Monotonic.INCREASING and is_point_above_line_vector(frame[3][0], frame[3][1], door.x1, door.y1, door.x2, door.y2):
+                        # 进门数+1
+                        flow.in_num += 1
+                    elif monotonic_type == Monotonic.DECREASING and is_point_below_line_vector(frame[1][0], frame[1][1], door.x1, door.y1, door.x2, door.y2):
+                        # 出门数+1
+                        flow.out_num += 1
                 case 3:
-                    pass
+                    if monotonic_type == Monotonic.INCREASING and is_point_above_line_vector(frame[3][0], frame[3][1], door.x1, door.y1, door.x2, door.y2):
+                        # 出门数+1
+                        flow.out_num += 1
+                    elif monotonic_type == Monotonic.DECREASING and is_point_below_line_vector(frame[1][0], frame[1][1], door.x1, door.y1, door.x2, door.y2):
+                        # 进门数+1
+                        flow.in_num += 1
 
 if __name__ == '__main__':
     banner()
-    
-    input_file = "virtual_data.json"
-    output_file = "compacted_data.json"
-    
+
+    # 读取门配置
+    pass
+    door = Door()
+    # 连接存储
+    pass
+    flow = Flow()
     # 执行转换
+
+    input_file = './data/virtual_data.json'
+    output_file = './data/compacted_data.json'
     result = compact(input_file, output_file)
+    counter(result, door, flow)
