f13334277d50ff8e5cef5b65b21b1bde6a1be527
jwinf_91 <13675006110@163.com>
2025-11-18 14:02:16 +0800

    fix: 修改main为入口

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-from flask_cors import CORS
-from controller.counter_controller import counter_bp
+from app.__init import create_app
 
-app = Flask(__name__)
-CORS(app)
-
-app.register_blueprint(counter_bp, url_prefix='/erkai/visualnet')
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=True)
